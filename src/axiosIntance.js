a34a1aa31e191331258f5e9324d064446936a7b3
Saad Raza <razasaad62186gmail.com>
2025-05-13 23:43:31 +0500

    solve problemss

--- src/axiosIntance.js
+++ src/axiosIntance.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-  baseURL: "https://ba-you-production.up.railway.app/api",
+  baseURL: "https://ba-you-production.up.railway.app",
   withCredentials: true,
   timeout: 30000,
   headers: {
